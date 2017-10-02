6837138d513000e7971574b8d701d1c24c7d2fa4
aschera <aschera@gmail.com>
2017-10-02 10:06:20 +0200

    webserver set up with mongodb, mongoos, express and pug. all working together: db content displayed on pug website

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Init App
+const app = express();
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function(err){
+  console.log(err);
+});
+
+
+// Bring in Models
+let Vehicle = require('./models/vehicle');
+
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+
+// Home Route
+app.get('/', function(req, res){
+  Vehicle.find({}, function(err, vehicles){
+      res.render('index', {
+        title:'Booking system',
+        vehicles: vehicles
+      });
+      });
+});
+
+
+// Start Server
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
+});
