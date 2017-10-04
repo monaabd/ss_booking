d1c94a92c05c9f9ad297ca6f93b35985de3b183b
MiraMai <mira.aeridou@gmail.com>
2017-10-04 20:24:47 +0200

    navigation & footer

---
+++ src/carlist.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Carlist extends Component {
+ 
+  render() {
+    return (
+        <div>
+            <p>list of cars </p>
+        </div>
+      
+    );
+  }
+    
+}
+
+export default Carlist;
---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Footer extends Component {
+    render() {
+        return (
+        <footer>This is the footer</footer>
+        );
+    }
+}
+    
+export default Footer;    
