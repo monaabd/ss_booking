ee33919944a4c38095acc9746b131798866769ed
lunapollo <rikard.mattsson@gmail.com>
2017-10-06 14:36:43 +0200

    added admin

--- vehiclebooking/src/components/admin.js
+++ vehiclebooking/src/components/admin.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import UpdateCar from '../components/updateCar';
 
 
 class Admin extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      cars: [],
+      chosenCar: null
+    };
+  this.printCars = this.printCars.bind(this);
+  this.chooseCar = this.chooseCar.bind(this);
+  }
+  componentDidMount() {
+    fetch('/vehicles')
+      .then(res => res.json())
+      .then(cars => this.setState({ cars }));
+    }
+    chooseCar(event){
+      let clickedCar = this.state.cars.find(car => {
+        return car._id === event.target.parentNode.id;
+      });
+      console.log(clickedCar);
+      this.setState({
+        chosenCar: clickedCar
+      });
+    }
+    printCars(){
+      return this.state.cars.map(car =>
+       <tr onClick={this.chooseCar} className="adminList" key={car._id} id={car._id}>
+        <td>Id: <b>{car._id}</b></td>
+        <td>Brand: <b>{car.brand}</b></td>
+        <td>Model: <b>{car.model}</b></td>
+        <td>Year: <b>{car.year}</b></td>
+       </tr>
+     )
+    }
   render() {
+    if (this.state.cars.length <= 0){
     return (
       <div>
         <h1>Admin view</h1>
+        <p>Loading cars...</p>
+      </div>
+    );
+    }
+    else return (
+      <div>
+        <h1>Vehicles view</h1>
+        <table>
+          <tbody>
+            {this.printCars()}
+          </tbody>
+        </table>
+        <UpdateCar
+          chosen={this.state.chosenCar}
+        />
       </div>
     );
   }
-
 }
 
 export default Admin;
--- vehiclebooking/src/components/vehicles.js
+++ vehiclebooking/src/components/vehicles.js
@@ -5,18 +5,39 @@ import '../css/App.css';
 class Vehicles extends Component {
   constructor(props){
     super(props);
+    this.state= {cars: []};
   this.apiRequest = this.apiRequest.bind(this);
   }
   apiRequest(){
   fetch('/vehicles')
     .then(res => res.json())
-    .then(users => console.log( users ));
+    .then(cars => this.setState({ cars }));
   }
+  componentDidMount() {
+    fetch('/vehicles')
+      .then(res => res.json())
+      .then(cars => this.setState({ cars }));
+    }
   render() {
+    if (this.state.cars.length <= 0){
     return (
       <div>
         <h1>Vehicles view</h1>
         <button onClick={this.apiRequest}>Click to request db from API</button>
+        <div>
+        <h1>Cars</h1>
+          </div>
+      </div>
+    );
+    }
+    else return (
+      <div>
+        <h1>Vehicles view</h1>
+        <button onClick={this.apiRequest}>Click to request db from API</button>
+        <div>
+        <h1>Cars</h1>
+        <div key={this.state.cars[0]._id}>{this.state.cars[0].brand}{this.state.cars[0].fordonstyp}</div>
+      </div>
       </div>
     );
   }
