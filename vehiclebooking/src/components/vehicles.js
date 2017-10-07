9eabf05309fc7832a0ba13700082c650b5f4473a
Mona Abdollahi <abd.mona@hotmail.com>
2017-10-07 11:31:29 +0200

    vehicle updates

--- vehiclebooking/src/components/vehicles.js
+++ vehiclebooking/src/components/vehicles.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import VehicleList from './vehicleList';
 
 
 class Vehicles extends Component {
+
   constructor(props){
     super(props);
+    this.state = {
+      vehicles : []
+    };
   this.apiRequest = this.apiRequest.bind(this);
+  this.update = this.update.bind(this);
   }
-  apiRequest(){
-  fetch('/vehicles', { 
-        headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
 
-    })
-    .then(res => res.json())
-    .then(users => console.log( users ));
-  }
+  apiRequest(){
+  const _this = this;
+  fetch('/vehicles')
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        _this.update(data);
+
+    for (var i = 0; i < data.length; i++) {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(' id: ' + data[i]._id );
+        node.appendChild(textnode);
+        document.getElementById("vehicles").appendChild(node);
+    } //end for loop
+    }) //end data function
+    } // end apiRequest
+
+update(data){
+this.setState({
+vehicles : [data]
+});
+console.log('vehicles:');
+console.log(this.state.vehicles);
+}
+
   render() {
     return (
-      <div>
-        <h1>Vehicles view</h1>
-        <button onClick={this.apiRequest}>Click to request db from API</button>
-      </div>
+        <VehicleList apiRequest={this.apiRequest} />
     );
   }
 
