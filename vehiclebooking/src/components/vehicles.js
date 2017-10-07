c7abdf9a37648674242cb4578ec595b00f199e4a
aschera <aschera@gmail.com>
2017-10-07 08:44:12 +0200

    added image and button to vehicle list

--- vehiclebooking/src/components/vehicles.js
+++ vehiclebooking/src/components/vehicles.js
@@ -23,10 +23,40 @@ class Vehicles extends Component {
         _this.update(data);
 
     for (var i = 0; i < data.length; i++) {
-        var node = document.createElement("LI");
+        // first make an li element for every car.
+        var listitem = document.createElement("LI");
+        listitem.id = data[i]._id;
+
+        // then make a div to hold the data
+        var box = document.createElement("DIV");
+        box.id = 'vehicleslistbox';
+        //->  put box inside li
+        listitem.appendChild(box);
+
+        // then make a div to hold the text.
+        var textbox = document.createElement("p");
+
+        // then make a div to hold the image.
+        var image = document.createElement("IMG");
+        // set image src to vehicle data
+        image.src = data[i].imgLink;
+        image.style.height = 50 + 'px';
+
+        // then make a button to go to booking.
+        var button = document.createElement("button");
+        button.innerHTML = 'Book';
+
+        // -> put textbox and image inside box
+        box.appendChild(textbox);
+        box.appendChild(image);
+        box.appendChild(button);
+
+        // then make a text string to display in the textbox.
         var textnode = document.createTextNode(' id: ' + data[i]._id );
-        node.appendChild(textnode);
-        document.getElementById("vehicles").appendChild(node);
+        // -> put text inside the textbox
+        textbox.appendChild(textnode);
+
+        document.getElementById("vehicles").appendChild(listitem);
     } //end for loop
     }) //end data function
     } // end apiRequest
