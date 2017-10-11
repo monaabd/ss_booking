7014add1fb2d048607f8634ca24caa0117168a9a
Mona Abdollahi <abd.mona@hotmail.com>
2017-10-11 09:06:51 +0200

    update

--- vehiclebooking/src/components/addButton.js
+++ vehiclebooking/src/components/addButton.js
@@ -4,12 +4,23 @@ import '../css/App.css';
 
 class AddButton extends Component {
   constructor(props){
-    super(props);
-    this.state = {
-    }
+   super(props);
+   this.apiAdd = this.apiAdd.bind(this);
+  }
+  apiAdd(){
+    //let car = this.props.newCar;
+    fetch('/vehicles', {
+      method: 'post',
+      body: JSON.stringify({
+
+	})
+});
+  }
   render() {
     return (
-      <button>Press me</button>
+      <div>
+        <button onClick={this.apiAdd}>ADD</button>
+      </div>
     );
   }
 }
--- vehiclebooking/src/components/updateCar.js
+++ vehiclebooking/src/components/updateCar.js
@@ -8,34 +8,38 @@ import AddButton from '../components/addButton';
 class UpdateCar extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      inputValues: {}
-    }
-  this.updateFordonstyp = this.updateFordonstyp.bind(this);
-  }
-  updateFordonstyp(event){
-    this.setState({
-      inputValues.fordonstyp: event.target.value;
-    });
   }
   render() {
+
     if (this.props.chosen === null || this.props.chosen === "" || this.props.chosen === undefined)
       return (
         <p>No car chosen</p>
     );
-    else return (
-      <input onChange={this.updateFordonstyp}>{this.props.chosen.fordonstyp} />
-      <button onClick={this.update}>Update values</button>
-      <UpdateButton
-        values={this.state.inputValues}
-      /> //this.props.values.fordontyp
-      <DeleteButton
-         carId={this.props.chosen._id}
-       />
-      <AddButton
-        values={this.state.inputValues}
-      />
-    );
+    else {
+      return (
+        <div>
+          <span>Vehicle type: </span><input id="i1" onChange={this.props.upCar} defaultValue={this.props.chosen.fordonstyp}  /><br/>
+          <span>Driving License: </span><input id="i2" onChange={this.props.upCar} defaultValue={this.props.chosen.requiredDrivingLicense} /><br/>
+          <span>Brand: </span><input id="i3" onChange={this.props.upCar} defaultValue={this.props.chosen.brand} /><br/>
+          <span>Model: </span><input id="i4" onChange={this.props.upCar} defaultValue={this.props.chosen.gearbox} /><br/>
+          <span>Year: </span><input id="i5" onChange={this.props.upCar} defaultValue={this.props.chosen.model} /><br/>
+          <span>Gearbox: </span><input id="i6" onChange={this.props.upCar} defaultValue={this.props.chosen.year} /><br/>
+          <span>Rent: </span><input id="i7" onChange={this.props.upCar} defaultValue={this.props.chosen.dagshyra} /><br/>
+          <span>Photo: </span><input id="i8" onChange={this.props.upCar} defaultValue={this.props.chosen.imgLink} /><br/>
+          <span>Fuel: </span><input id="i9" onChange={this.props.upCar} defaultValue={this.props.chosen.fuel} /><br/>
+
+          <UpdateButton
+            newCar={this.props.chosen}
+          />
+          <DeleteButton
+             carId={this.props.chosen._id}
+           />
+        /*  <AddButton
+            newCar={this.props.chosen}
+          />*/
+        </div>
+      );
+    }
   }
 }
 
