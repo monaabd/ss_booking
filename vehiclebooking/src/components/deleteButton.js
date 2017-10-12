8ad018fcf989b663978ea79d578875e58e5c44b8
Mona Abdollahi <abd.mona@hotmail.com>
2017-10-12 11:27:18 +0200

    deletebutton and checkbox del1

--- vehiclebooking/src/components/deleteButton.js
+++ vehiclebooking/src/components/deleteButton.js
@@ -5,23 +5,17 @@ import '../css/App.css';
 class DeleteButton extends Component {
   constructor(props){
     super(props);
-
   this.clickDelete = this.clickDelete.bind(this);
 }
-
-
  clickDelete() {
+    var DelComponent = this;
     let item = JSON.stringify(this.props.carId);
-    return fetch("/vehicles" + '/' + item, {
-    method: "DELETE",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).then(response => response.json());
-     console.log(this.props.carId);
-     this.props.printMsg(""+this.props.carId);
-
+    console.log(item);
+    fetch("/vehicles" + '/' + item, {
+    method: "DELETE"
+    }).then(response =>{
+     DelComponent.props.printMsg("Vehicle with Id: "+item+" successfully deleted")
+   });
 }
   render() {
     return (
