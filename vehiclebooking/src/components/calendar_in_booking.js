d2089b46092bfa6a6dd047385b731bd34fc958f7
aschera <aschera@gmail.com>
2017-10-13 12:31:06 +0200

    d

--- vehiclebooking/src/components/calendar_in_booking.js
+++ vehiclebooking/src/components/calendar_in_booking.js
@@ -45,8 +45,7 @@ handleChangeEndX(date) {
     this.setState({
       endDateX: date,
     });
-    this.props.changeTo(date._d);
-    console.log(date._d);
+
     //let thedate = JSON.stringify(date);
     //thedate = thedate.slice(0, 11);
 
